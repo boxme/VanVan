1c4f585ab50113b69a042d8d65565be27ef5fc38
desmond <desmond@replaid.com>
2015-10-15 00:03:39 +0800

    Add compan table schema & relations

--- data/schema.js
+++ data/schema.js
@@ -13,11 +13,19 @@ var schema = {
 	driver: {
 		id: {type: 'increments', nullable: false, primary: true},
 		name: {type: 'string', maxlength: 150, nullable: false},
-		email: {type: 'string', maxlength: 254, nullable: false, unique: true},
+		email: {type: 'string', maxlength: 254, nullable: true, unique: false},
 		password: {type: 'string', nullable: false},
 		mobile: {type: 'string', nullable: false, unique: true},
-		companyId: {type: 'integer', nullable: true, unique: false},
+		company_id: {type: 'integer', nullable: true, unique: false},
 		token: {type: 'string', nullable: true}
+	},
+
+	company: {
+		id: {type: 'increments', nullable: false, primary: true},
+		name: {type: 'string', maxlength: 150, nullable: false, unique: true},
+		email: {type: 'string', maxlength: 254, nullable: false, unique: true},
+		address: {type: 'string', maxlength: 254, nullable: false},
+		mobile: {type: 'string', nullable: false, unique: true}
 	}
 };
 
--- models/index.js
+++ models/index.js
@@ -18,6 +18,25 @@ bookshelf.plugin('visibility');
 var user = bookshelf.Model.extend({
 	tableName: 'user'
 });
+
 exports.user = user;
+
+var driver = bookshelf.Model.extend({
+	tableName: 'driver',
+	company: function() {
+		// one-to-one or many-to-one
+		return this.belongsTo(company, 'company_id');
+	}
+});
+
+exports.driver = driver;
+
+var company = bookshelf.Model.extend({
+	tableName: 'company',
+	driver: function() {
+		// one-to-many
+		this.hasMany(driver, "driver_id");
+	}
+});
 
 exports.bookshelf = bookshelf;

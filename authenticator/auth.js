27a2cfca777ff2f88441eb261331d24f848ac09c
desmond <desmond@replaid.com>
2015-10-23 00:41:48 +0800

    authenticator for user token (incomplete)

---
+++ authenticator/auth.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var authController = {};
+
+// token type
+var tokenType = {
+	NONE: 'NONE_TOKEN',
+	BODY: 'BODY_TOKEN',
+	QUERY: 'QUERY_TOKEN',
+	HEADER: 'HEADER_TOKEN'
+};
+
+authController.requireUser = function requireUser() {
+	return function checkForUser(req, res, next) {
+		
+		var tokenMethod = tokenType.NONE;
+
+		if (req.body.token) {
+			token = req.body.token;
+			tokenMethod = tokenType.BODY;
+		} else if (req.query.token) {
+			token = req.query.token;
+			tokenMethod = tokenType.QUERY;
+		} else if (req.headers.token) {
+			token = req.headers.token;
+			tokenMethod = tokenType.HEADER;
+		}
+
+		console.log('Auth method: ' + tokenMethod + ' - ' + req.url);
+
+		if (token) {
+			// Check for validity of token and then next() if it's correct
+		} else {
+			res.status(400).json({error: "require_token"});
+		}
+	};
+};
+
+module.exports = authController;
